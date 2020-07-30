d337875843991fc879032dfc474c3a9c3cc23640
Sweknas <mattias.sundberg@outlook.com>
2020-07-30 16:01:40 +0200

    new build

---
+++ new/precache-manifest.e7a87f156a81c7139916ae5c106fe17e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cadfd9c5ea60a4bbb5e5be16a515696",
+    "url": "/new/index.html"
+  },
+  {
+    "revision": "99a0612530ce9fe2c4d2",
+    "url": "/new/static/css/main.8c1b1180.chunk.css"
+  },
+  {
+    "revision": "d9149f179cf915bbb132",
+    "url": "/new/static/js/2.9b076428.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/new/static/js/2.9b076428.chunk.js.LICENSE"
+  },
+  {
+    "revision": "99a0612530ce9fe2c4d2",
+    "url": "/new/static/js/main.4a3e06d2.chunk.js"
+  },
+  {
+    "revision": "f0d3a09582f7494ad3df",
+    "url": "/new/static/js/runtime-main.c4a46060.js"
+  },
+  {
+    "revision": "fa2c88c906445f4b564d7f09bab4e761",
+    "url": "/new/static/media/Fidelity_bertram.fa2c88c9.pdf"
+  },
+  {
+    "revision": "fd07900ce5a54b253e75be3abe36c5f8",
+    "url": "/new/static/media/Test-from-FIDELITY-34-Norway-Bertram-Cables.fd07900c.pdf"
+  },
+  {
+    "revision": "2c5a95f994df607d712aa778f9a2e4a3",
+    "url": "/new/static/media/Test-from-FIDELITY-Norway-Bertram-is-best.2c5a95f9.pdf"
+  },
+  {
+    "revision": "b85fcee392fa9d481243f0d45b7cc231",
+    "url": "/new/static/media/bgLarge.b85fcee3.png"
+  },
+  {
+    "revision": "f1b15a17229bfba76aed75cfe20669d3",
+    "url": "/new/static/media/comingsoon.f1b15a17.png"
+  },
+  {
+    "revision": "6c0ac707010c2e501828b17eb66549dc",
+    "url": "/new/static/media/hifi4all.6c0ac707.pdf"
+  },
+  {
+    "revision": "4a0dfe6b77990ca7ed43278cd56d1426",
+    "url": "/new/static/media/logo5.4a0dfe6b.png"
+  },
+  {
+    "revision": "cd802a3118692c91e20f396ee43b6d58",
+    "url": "/new/static/media/logo5blue.cd802a31.png"
+  },
+  {
+    "revision": "c4f89f111e188241c4f534a7feca8589",
+    "url": "/new/static/media/proxima-c-2.c4f89f11.jpg"
+  },
+  {
+    "revision": "1b2413aa5f677877e16ab33c5fc8e3ea",
+    "url": "/new/static/media/proxima-c-main.1b2413aa.jpg"
+  },
+  {
+    "revision": "780bce8bbf0ff888153150d4e68ef3bb",
+    "url": "/new/static/media/ultima-2.780bce8b.png"
+  },
+  {
+    "revision": "bfd4ccf961cceb855e69d09f2d861d3e",
+    "url": "/new/static/media/ultima-3.bfd4ccf9.png"
+  },
+  {
+    "revision": "f66cef7be2400d115095acf9b3be5ef6",
+    "url": "/new/static/media/ultima-main.f66cef7b.png"
+  }
+]);
